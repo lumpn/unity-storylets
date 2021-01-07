a51854846265cb11b4071318883e68f4524577b5
Jonas Boetel <lumpn@users.noreply.github.com>
2021-01-07 12:11:11 +0800

    Todo: rule clustering

--- Assets/Scripts/Editor/Tests/PerformanceTest.cs
+++ Assets/Scripts/Editor/Tests/PerformanceTest.cs
@@ -11,18 +11,23 @@ namespace Test
         [Test]
         public void ManyRules()
         {
-            const int numRules = 1000000;
+            const int numRules = 1000 * 1000;
 
             var ruleset = new RulesetBuilder();
 
             var effect = new LogEffect("Rule executing");
 
+            Profiler.BeginSample("Ruleset");
             for (int i = 0; i < numRules; i++)
             {
                 var rule = ruleset.AddRule(effect);
                 rule.AddPredicate(i).EqualTo(10);
             }
+            Profiler.EndSample();
+
+            Profiler.BeginSample("Compile");
             var compiledRuleset = ruleset.Build();
+            Profiler.EndSample();
 
             var state = new StateBuilder();
             var variable = state.AddVariable(numRules / 2);
@@ -35,14 +40,14 @@ namespace Test
 
             {
                 Profiler.BeginSample("State1");
-                var rule = compiledRuleset.Query(compiledState1);
+                var rule = compiledRuleset.Query(compiledState1); // ~150ms
                 Profiler.EndSample();
                 Assert.IsNull(rule);
             }
 
             {
                 Profiler.BeginSample("State2");
-                var rule = compiledRuleset.Query(compiledState2);
+                var rule = compiledRuleset.Query(compiledState2); // ~75ms
                 Profiler.EndSample();
                 Assert.IsNotNull(rule);
             }
